ca1ddafbe9b756fb1cfcaf42719c3957ed42259a
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-08-02 21:03:41 +0200

    scrapping is working now

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@
 const puppet = require("puppeteer");
 
 (async () => {
-    let browser = puppet.launch({headless: false});
+    let browser = await puppet.launch({headless: false});
     let page = await (await browser).newPage();
 
-    await page.goto("https://www.facebook.com/groups/959750777535536").catch((reason) => {
-      console.log(reason);
-    })
-    await page.waitForSelector('[aria-label="News Feed"]').catch((reason) => {
-      console.log(reason);
+    await page.goto("https://www.facebook.com/groups/959750777535536", {waitUntil: 'domcontentloaded'}).finally(() => {
+      console.log("finished");
     });
-    console.log("lodede");
+    await page.waitForSelector('[aria-label="News Feed"]');
+    const elms = await page.evaluate(() => {
+      const posts = Array.from(document.querySelectorAll('[id^="mall_post"]'));
+      return posts.map((post) => {
+        const arrays = post.innerText.split('\n');
+        return arrays.slice(3, 4);
+      });
+    });
+
+    console.log(elms);
 })();
